c3b049416887e7694fbb645bd0f5923e812b2c5e
raavimanikanta <92731330+raavimanikanta@users.noreply.github.com>
2024-03-01 01:11:47 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,18 @@
 const getDetails=(req,res)=>{
-    res.send({
-        name:"manikanta"
-    })
+    res.send([
+{
+SkuPartNumber: "DYN365_BUSCENTRAL_TEAM_MEMBER",
+skuId: "2e3c4023-80f6-4711-aa5d-29e0ecb46835",
+
+},
+{
+SkuPartNumber: "Teams_Premium_(for_Departments)",
+Name: "Teams Premium (for Departments)",
+"Teams_Premium_(for_Departments)_Total": "0",
+"Teams_Premium_(for_Departments)_Assigned": "0",
+"Teams_Premium_(for_Departments)_Available": "0"
+}
+])
 }
 
 module.exports.getDetails=getDetails
