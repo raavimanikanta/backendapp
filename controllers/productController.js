bc693f72ed9d8945467a8b1114b4fe2f0dbe56f9
raavimanikanta <92731330+raavimanikanta@users.noreply.github.com>
2024-03-04 15:19:49 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,192 +1,281 @@
 const getDetails=(req,res)=>{
-    res.send(
-        [
-{
-"SkuPartNumber": "VISIOCLIENT",
-"skuId": "c5928f49-12ba-48f7-ada3-0d743a3601d5",
-"Name": "Visio Plan 2",
-"VISIOCLIENT_Total": "5",
-"VISIOCLIENT_Assigned": "5",
-"VISIOCLIENT_Available": "0"
-},
-{
-"SkuPartNumber": "STREAM",
-"skuId": "1f2f344a-700d-42c9-9427-5cea1d5d7ba6",
-"Name": "Microsoft Stream Trial",
-"STREAM_Total": "1000000",
-"STREAM_Assigned": "2",
-"STREAM_Available": "999998"
-},
-{
-"SkuPartNumber": "POWER_BI_PRO",
-"skuId": "f8a1db68-be16-40ed-86d5-cb42ce701560",
-"Name": "Power BI Pro",
-"POWER_BI_PRO_Total": "125",
-"POWER_BI_PRO_Assigned": "24",
-"POWER_BI_PRO_Available": "101"
-},
-{
-"SkuPartNumber": "DYN365_BUSCENTRAL_ESSENTIAL",
-"skuId": "2880026b-2b0c-4251-8656-5d41ff11e3aa",
-"Name": "Dynamics 365 Business Central Essentials",
-"DYN365_BUSCENTRAL_ESSENTIAL_Total": "3",
-"DYN365_BUSCENTRAL_ESSENTIAL_Assigned": "5",
-"DYN365_BUSCENTRAL_ESSENTIAL_Available": "-2"
-},
-{
-"SkuPartNumber": "WINDOWS_STORE",
-"skuId": "6470687e-a428-4b7a-bef2-8a291ad947c9",
-"Name": "WINDOWS_STORE",
-"WINDOWS_STORE_Total": "1000000",
-"WINDOWS_STORE_Assigned": "0",
-"WINDOWS_STORE_Available": "1000000"
-},
-{
-"SkuPartNumber": "FLOW_FREE",
-"skuId": "f30db892-07e9-47e9-837c-80727f46fd3d",
-"Name": "Microsoft Power Automate Free",
-"FLOW_FREE_Total": "10000",
-"FLOW_FREE_Assigned": "211",
-"FLOW_FREE_Available": "9789"
-},
-{
-"SkuPartNumber": "Viva_Goals_User_led",
-"skuId": "3a349c99-ffec-43d2-a2e8-6b97fcb71103",
-"Name": "Viva Goals User-led",
-"Viva_Goals_User_led_Total": "20",
-"Viva_Goals_User_led_Assigned": "18",
-"Viva_Goals_User_led_Available": "2"
-},
-{
-"SkuPartNumber": "MCOPSTN1",
-"skuId": "0dab259f-bf13-4952-b7f8-7db8f131b28d",
-"Name": "Microsoft Teams Domestic Calling Plan",
-"MCOPSTN1_Total": "5",
-"MCOPSTN1_Assigned": "4",
-"MCOPSTN1_Available": "1"
-},
-{
-"SkuPartNumber": "PROJECTPREMIUM",
-"skuId": "09015f9f-377f-4538-bbb5-f75ceb09358a",
-"Name": "Project Plan 5",
-"PROJECTPREMIUM_Total": "20",
-"PROJECTPREMIUM_Assigned": "12",
-"PROJECTPREMIUM_Available": "8"
-},
-{
-"SkuPartNumber": "CCIBOTS_PRIVPREV_VIRAL",
-"skuId": "606b54a9-78d8-4298-ad8b-df6ef4481c80",
-"Name": "Microsoft Copilot Studio Viral Trial",
-"CCIBOTS_PRIVPREV_VIRAL_Total": "10000",
-"CCIBOTS_PRIVPREV_VIRAL_Assigned": "3",
-"CCIBOTS_PRIVPREV_VIRAL_Available": "9997"
-},
-{
-"SkuPartNumber": "POWERAPPS_VIRAL",
-"skuId": "dcb1a3ae-b33f-4487-846a-a640262fadf4",
-"Name": "Microsoft Power Apps Plan 2 Trial",
-"POWERAPPS_VIRAL_Total": "10000",
-"POWERAPPS_VIRAL_Assigned": "10",
-"POWERAPPS_VIRAL_Available": "9990"
-},
-{
-"SkuPartNumber": "POWER_BI_STANDARD",
-"skuId": "a403ebcc-fae0-4ca2-8c8c-7a907fd6c235",
-"Name": "Microsoft Fabric (Free)",
-"POWER_BI_STANDARD_Total": "1000000",
-"POWER_BI_STANDARD_Assigned": "122",
-"POWER_BI_STANDARD_Available": "999878"
-},
-{
-"SkuPartNumber": "MCOPSTNC",
-"skuId": "47794cd0-f0e5-45c5-9033-2eb6b5fc84e0",
-"Name": "Communications Credits",
-"MCOPSTNC_Total": "10000000",
-"MCOPSTNC_Assigned": "0",
-"MCOPSTNC_Available": "10000000"
-},
-{
-"SkuPartNumber": "PBI_PREMIUM_PER_USER",
-"skuId": "c1d032e0-5619-4761-9b5c-75b6831e1711",
-"Name": "Power BI Premium Per User",
-"PBI_PREMIUM_PER_USER_Total": "1",
-"PBI_PREMIUM_PER_USER_Assigned": "1",
-"PBI_PREMIUM_PER_USER_Available": "0"
-},
-{
-"SkuPartNumber": "DYN365_FINANCIALS_ACCOUNTANT_SKU",
-"skuId": "9a1e33ed-9697-43f3-b84c-1b0959dbb1d4",
-"Name": "Dynamics 365 Business Central External Accountant",
-"DYN365_FINANCIALS_ACCOUNTANT_SKU_Total": "1",
-"DYN365_FINANCIALS_ACCOUNTANT_SKU_Assigned": "1",
-"DYN365_FINANCIALS_ACCOUNTANT_SKU_Available": "0"
-},
-{
-"SkuPartNumber": "SPE_E5",
-"skuId": "06ebc4ee-1bb5-47dd-8120-11324bc54e06",
-"Name": "Microsoft 365 E5",
-"SPE_E5_Total": "125",
-"SPE_E5_Assigned": "120",
-"SPE_E5_Available": "5"
-},
-{
-"SkuPartNumber": "POWERAPPS_PER_USER",
-"skuId": "b30411f5-fea1-4a59-9ad9-3db7c7ead579",
-"Name": "Power Apps Premium",
-"POWERAPPS_PER_USER_Total": "1",
-"POWERAPPS_PER_USER_Assigned": "1",
-"POWERAPPS_PER_USER_Available": "0"
-},
-{
-"SkuPartNumber": "DYN365_BUSCENTRAL_ADD_ENV_ADDON",
-"skuId": "a58f5506-b382-44d4-bfab-225b2fbf8390",
-"Name": "DYN365_BUSCENTRAL_ADD_ENV_ADDON",
-"DYN365_BUSCENTRAL_ADD_ENV_ADDON_Total": "2",
-"DYN365_BUSCENTRAL_ADD_ENV_ADDON_Assigned": "0",
-"DYN365_BUSCENTRAL_ADD_ENV_ADDON_Available": "2"
-},
-{
-"SkuPartNumber": "SPE_E3",
-"skuId": "05e9a617-0261-4cee-bb44-138d3ef5d965",
-"Name": "Microsoft 365 E3",
-"SPE_E3_Total": "315",
-"SPE_E3_Assigned": "315",
-"SPE_E3_Available": "0"
-},
-{
-"SkuPartNumber": "DYN365_BUSCENTRAL_TEAM_MEMBER",
-"skuId": "2e3c4023-80f6-4711-aa5d-29e0ecb46835",
-"Name": "Dynamics 365 Business Central Team Members",
-"DYN365_BUSCENTRAL_TEAM_MEMBER_Total": "1",
-"DYN365_BUSCENTRAL_TEAM_MEMBER_Assigned": "1",
-"DYN365_BUSCENTRAL_TEAM_MEMBER_Available": "0"
-},
-{
-"SkuPartNumber": "Teams_Premium_(for_Departments)",
-"skuId": "52ea0e27-ae73-4983-a08f-13561ebdb823",
-"Name": "Teams Premium (for Departments)",
-"Teams_Premium_(for_Departments)_Total": "0",
-"Teams_Premium_(for_Departments)_Assigned": "0",
-"Teams_Premium_(for_Departments)_Available": "0"
-},
-{
-"SkuPartNumber": "PROJECT_MADEIRA_PREVIEW_IW_SKU",
-"skuId": "6a4a1628-9b9a-424d-bed5-4118f0ede3fd",
-"Name": "Dynamics 365 Business Central for IWs",
-"PROJECT_MADEIRA_PREVIEW_IW_SKU_Total": "10000",
-"PROJECT_MADEIRA_PREVIEW_IW_SKU_Assigned": "0",
-"PROJECT_MADEIRA_PREVIEW_IW_SKU_Available": "10000"
-},
-{
-"SkuPartNumber": "POWERAPPS_DEV",
-"skuId": "5b631642-bd26-49fe-bd20-1daaa972ef80",
-"Name": "Microsoft PowerApps for Developer",
-"POWERAPPS_DEV_Total": "10000",
-"POWERAPPS_DEV_Assigned": "5",
-"POWERAPPS_DEV_Available": "9995"
-}
-]
-    )
+    res.send([
+    {
+        "SkuPartNumber": "VISIOCLIENT",
+        "skuId": "c5928f49-12ba-48f7-ada3-0d743a3601d5",
+        "Name": "Visio Plan 2",
+        "VISIOCLIENT_Total": "5",
+        "VISIOCLIENT_Assigned": "5",
+        "VISIOCLIENT_Available": "0"
+    },
+    {
+        "SkuPartNumber": "STREAM",
+        "skuId": "1f2f344a-700d-42c9-9427-5cea1d5d7ba6",
+        "Name": "Microsoft Stream Trial",
+        "STREAM_Total": "1000000",
+        "STREAM_Assigned": "2",
+        "STREAM_Available": "999998"
+    },
+    {
+        "SkuPartNumber": "POWER_BI_PRO",
+        "skuId": "f8a1db68-be16-40ed-86d5-cb42ce701560",
+        "Name": "Power BI Pro",
+        "POWER_BI_PRO_Total": "125",
+        "POWER_BI_PRO_Assigned": "24",
+        "POWER_BI_PRO_Available": "101"
+    },
+    {
+        "SkuPartNumber": "DYN365_BUSCENTRAL_ESSENTIAL",
+        "skuId": "2880026b-2b0c-4251-8656-5d41ff11e3aa",
+        "Name": "Dynamics 365 Business Central Essentials",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Total": "3",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Assigned": "5",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Available": "-2"
+    },
+    {
+        "SkuPartNumber": "WINDOWS_STORE",
+        "skuId": "6470687e-a428-4b7a-bef2-8a291ad947c9",
+        "Name": "WINDOWS_STORE",
+        "WINDOWS_STORE_Total": "1000000",
+        "WINDOWS_STORE_Assigned": "0",
+        "WINDOWS_STORE_Available": "1000000"
+    },
+    {
+        "SkuPartNumber": "FLOW_FREE",
+        "skuId": "f30db892-07e9-47e9-837c-80727f46fd3d",
+        "Name": "Microsoft Power Automate Free",
+        "FLOW_FREE_Total": "10000",
+        "FLOW_FREE_Assigned": "211",
+        "FLOW_FREE_Available": "9789"
+    },
+    {
+        "SkuPartNumber": "Viva_Goals_User_led",
+        "skuId": "3a349c99-ffec-43d2-a2e8-6b97fcb71103",
+        "Name": "Viva Goals User-led",
+        "Viva_Goals_User_led_Total": "20",
+        "Viva_Goals_User_led_Assigned": "18",
+        "Viva_Goals_User_led_Available": "2"
+    },
+    {
+        "SkuPartNumber": "MCOPSTN1",
+        "skuId": "0dab259f-bf13-4952-b7f8-7db8f131b28d",
+        "Name": "Microsoft Teams Domestic Calling Plan",
+        "MCOPSTN1_Total": "5",
+        "MCOPSTN1_Assigned": "4",
+        "MCOPSTN1_Available": "1"
+    },
+    {
+        "SkuPartNumber": "PROJECTPREMIUM",
+        "skuId": "09015f9f-377f-4538-bbb5-f75ceb09358a",
+        "Name": "Project Plan 5",
+        "PROJECTPREMIUM_Total": "20",
+        "PROJECTPREMIUM_Assigned": "12",
+        "PROJECTPREMIUM_Available": "8"
+    },
+    {
+        "SkuPartNumber": "CCIBOTS_PRIVPREV_VIRAL",
+        "skuId": "606b54a9-78d8-4298-ad8b-df6ef4481c80",
+        "Name": "Microsoft Copilot Studio Viral Trial",
+        "CCIBOTS_PRIVPREV_VIRAL_Total": "10000",
+        "CCIBOTS_PRIVPREV_VIRAL_Assigned": "3",
+        "CCIBOTS_PRIVPREV_VIRAL_Available": "9997"
+    },
+    {
+        "SkuPartNumber": "POWERAPPS_VIRAL",
+        "skuId": "dcb1a3ae-b33f-4487-846a-a640262fadf4
+User
+return me that complete object by adding double quotes in every attribute
+ChatGPT
+Sure, here's the complete JSON object with double quotes added to every attribute:
+
+json
+Copy code
+[
+    {
+        "SkuPartNumber": "VISIOCLIENT",
+        "skuId": "c5928f49-12ba-48f7-ada3-0d743a3601d5",
+        "Name": "Visio Plan 2",
+        "VISIOCLIENT_Total": "5",
+        "VISIOCLIENT_Assigned": "5",
+        "VISIOCLIENT_Available": "0"
+    },
+    {
+        "SkuPartNumber": "STREAM",
+        "skuId": "1f2f344a-700d-42c9-9427-5cea1d5d7ba6",
+        "Name": "Microsoft Stream Trial",
+        "STREAM_Total": "1000000",
+        "STREAM_Assigned": "2",
+        "STREAM_Available": "999998"
+    },
+    {
+        "SkuPartNumber": "POWER_BI_PRO",
+        "skuId": "f8a1db68-be16-40ed-86d5-cb42ce701560",
+        "Name": "Power BI Pro",
+        "POWER_BI_PRO_Total": "125",
+        "POWER_BI_PRO_Assigned": "24",
+        "POWER_BI_PRO_Available": "101"
+    },
+    {
+        "SkuPartNumber": "DYN365_BUSCENTRAL_ESSENTIAL",
+        "skuId": "2880026b-2b0c-4251-8656-5d41ff11e3aa",
+        "Name": "Dynamics 365 Business Central Essentials",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Total": "3",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Assigned": "5",
+        "DYN365_BUSCENTRAL_ESSENTIAL_Available": "-2"
+    },
+    {
+        "SkuPartNumber": "WINDOWS_STORE",
+        "skuId": "6470687e-a428-4b7a-bef2-8a291ad947c9",
+        "Name": "WINDOWS_STORE",
+        "WINDOWS_STORE_Total": "1000000",
+        "WINDOWS_STORE_Assigned": "0",
+        "WINDOWS_STORE_Available": "1000000"
+    },
+    {
+        "SkuPartNumber": "FLOW_FREE",
+        "skuId": "f30db892-07e9-47e9-837c-80727f46fd3d",
+        "Name": "Microsoft Power Automate Free",
+        "FLOW_FREE_Total": "10000",
+        "FLOW_FREE_Assigned": "211",
+        "FLOW_FREE_Available": "9789"
+    },
+    {
+        "SkuPartNumber": "Viva_Goals_User_led",
+        "skuId": "3a349c99-ffec-43d2-a2e8-6b97fcb71103",
+        "Name": "Viva Goals User-led",
+        "Viva_Goals_User_led_Total": "20",
+        "Viva_Goals_User_led_Assigned": "18",
+        "Viva_Goals_User_led_Available": "2"
+    },
+    {
+        "SkuPartNumber": "MCOPSTN1",
+        "skuId": "0dab259f-bf13-4952-b7f8-7db8f131b28d",
+        "Name": "Microsoft Teams Domestic Calling Plan",
+        "MCOPSTN1_Total": "5",
+        "MCOPSTN1_Assigned": "4",
+        "MCOPSTN1_Available": "1"
+    },
+    {
+        "SkuPartNumber": "PROJECTPREMIUM",
+        "skuId": "09015f9f-377f-4538-bbb5-f75ceb09358a",
+        "Name": "Project Plan 5",
+        "PROJECTPREMIUM_Total": "20",
+        "PROJECTPREMIUM_Assigned": "12",
+        "PROJECTPREMIUM_Available": "8"
+    },
+    {
+        "SkuPartNumber": "CCIBOTS_PRIVPREV_VIRAL",
+        "skuId": "606b54a9-78d8-4298-ad8b-df6ef4481c80",
+        "Name": "Microsoft Copilot Studio Viral Trial",
+        "CCIBOTS_PRIVPREV_VIRAL_Total": "10000",
+        "CCIBOTS_PRIVPREV_VIRAL_Assigned": "3",
+        "CCIBOTS_PRIVPREV_VIRAL_Available": "9997"
+    },
+    {
+        "SkuPartNumber": "POWERAPPS_VIRAL",
+        "skuId": "dcb1a3ae-b33f-4487-846a-a640262fadf4",
+        "Name": "Microsoft Power Apps Plan 2 Trial",
+        "POWERAPPS_VIRAL_Total": "10000",
+        "POWERAPPS_VIRAL_Assigned": "10",
+        "POWERAPPS_VIRAL_Available": "9990"
+    },
+    {
+        "SkuPartNumber": "POWER_BI_STANDARD",
+        "skuId": "a403ebcc-fae0-4ca2-8c8c-7a907fd6c235",
+        "Name": "Microsoft Fabric (Free)",
+        "POWER_BI_STANDARD_Total": "1000000",
+        "POWER_BI_STANDARD_Assigned": "122",
+        "POWER_BI_STANDARD_Available": "999878"
+    },
+    {
+        "SkuPartNumber": "MCOPSTNC",
+        "skuId": "47794cd0-f0e5-45c5-9033-2eb6b5fc84e0",
+        "Name": "Communications Credits",
+        "MCOPSTNC_Total": "10000000",
+        "MCOPSTNC_Assigned": "0",
+        "MCOPSTNC_Available": "10000000"
+    },
+    {
+        "SkuPartNumber": "PBI_PREMIUM_PER_USER",
+        "skuId": "c1d032e0-5619-4761-9b5c-75b6831e1711",
+        "Name": "Power BI Premium Per User",
+        "PBI_PREMIUM_PER_USER_Total": "1",
+        "PBI_PREMIUM_PER_USER_Assigned": "1",
+        "PBI_PREMIUM_PER_USER_Available": "0"
+    },
+    {
+        "SkuPartNumber": "DYN365_FINANCIALS_ACCOUNTANT_SKU",
+        "skuId": "9a1e33ed-9697-43f3-b84c-1b0959dbb1d4",
+        "Name": "Dynamics 365 Business Central External Accountant",
+        "DYN365_FINANCIALS_ACCOUNTANT_SKU_Total": "1",
+        "DYN365_FINANCIALS_ACCOUNTANT_SKU_Assigned": "1",
+        "DYN365_FINANCIALS_ACCOUNTANT_SKU_Available": "0"
+    },
+    {
+        "SkuPartNumber": "SPE_E5",
+        "skuId": "06ebc4ee-1bb5-47dd-8120-11324bc54e06",
+        "Name": "Microsoft 365 E5",
+        "SPE_E5_Total": "125",
+        "SPE_E5_Assigned": "120",
+        "SPE_E5_Available": "5"
+    },
+    {
+        "SkuPartNumber": "POWERAPPS_PER_USER",
+        "skuId": "b30411f5-fea1-4a59-9ad9-3db7c7ead579",
+        "Name": "Power Apps Premium",
+        "POWERAPPS_PER_USER_Total": "1",
+        "POWERAPPS_PER_USER_Assigned": "1",
+        "POWERAPPS_PER_USER_Available": "0"
+    },
+    {
+        "SkuPartNumber": "DYN365_BUSCENTRAL_ADD_ENV_ADDON",
+        "skuId": "a58f5506-b382-44d4-bfab-225b2fbf8390",
+        "Name": "DYN365_BUSCENTRAL_ADD_ENV_ADDON",
+        "DYN365_BUSCENTRAL_ADD_ENV_ADDON_Total": "2",
+        "DYN365_BUSCENTRAL_ADD_ENV_ADDON_Assigned": "0",
+        "DYN365_BUSCENTRAL_ADD_ENV_ADDON_Available": "2"
+    },
+    {
+        "SkuPartNumber": "SPE_E3",
+        "skuId": "05e9a617-0261-4cee-bb44-138d3ef5d965",
+        "Name": "Microsoft 365 E3",
+        "SPE_E3_Total": "315",
+        "SPE_E3_Assigned": "315",
+        "SPE_E3_Available": "0"
+    },
+    {
+        "SkuPartNumber": "DYN365_BUSCENTRAL_TEAM_MEMBER",
+        "skuId": "2e3c4023-80f6-4711-aa5d-29e0ecb46835",
+        "Name": "Dynamics 365 Business Central Team Members",
+        "DYN365_BUSCENTRAL_TEAM_MEMBER_Total": "1",
+        "DYN365_BUSCENTRAL_TEAM_MEMBER_Assigned": "1",
+        "DYN365_BUSCENTRAL_TEAM_MEMBER_Available": "0"
+    },
+    {
+        "SkuPartNumber": "Teams_Premium_(for_Departments)",
+        "skuId": "52ea0e27-ae73-4983-a08f-13561ebdb823",
+        "Name": "Teams Premium (for Departments)",
+        "Teams_Premium_(for_Departments)_Total": "0",
+        "Teams_Premium_(for_Departments)_Assigned": "0",
+        "Teams_Premium_(for_Departments)_Available": "0"
+    },
+    {
+        "SkuPartNumber": "PROJECT_MADEIRA_PREVIEW_IW_SKU",
+        "skuId": "6a4a1628-9b9a-424d-bed5-4118f0ede3fd",
+        "Name": "Dynamics 365 Business Central for IWs",
+        "PROJECT_MADEIRA_PREVIEW_IW_SKU_Total": "10000",
+        "PROJECT_MADEIRA_PREVIEW_IW_SKU_Assigned": "0",
+        "PROJECT_MADEIRA_PREVIEW_IW_SKU_Available": "10000"
+    },
+    {
+        "SkuPartNumber": "POWERAPPS_DEV",
+        "skuId": "5b631642-bd26-49fe-bd20-1daaa972ef80",
+        "Name": "Microsoft PowerApps for Developer",
+        "POWERAPPS_DEV_Total": "10000",
+        "POWERAPPS_DEV_Assigned": "5",
+        "POWERAPPS_DEV_Available": "9995"
+    }
+])
 }
 
 module.exports.getDetails=getDetails
